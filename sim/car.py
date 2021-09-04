c0521307be91f73e67ca744318b48d258c20f626
sss04 <sssai@andrew.cmu.edu>
2021-09-04 17:41:12 -0400

    Add car + lane interaction logic

--- sim/car.py
+++ sim/car.py
@@ -3,33 +3,33 @@ from vehicle import Vehicle
 from consts import *
 
 class Car (Vehicle):
-    def __init__(self, rear_left: Tuple[float], height: float, width: float, direction: Direction) -> None:
+    def __init__(self, rear_left: Tuple[float], height: float, width: float) -> None:
         super().__init__()
         self.rear_left = rear_left
         self.speed = 0
         self.plan = []
         self.height = height
         self.width = width
-        self.direction = direction
-        self.findBoundaries()
+        self.lane = None
 
     def findBoundaries(self):
         # Add center calculations self.center
         rlX : float = self.rear_left[0]
         rlY : float = self.rear_left[1]
-        if self.direction == Direction.north:
+        assert self.lane is not None, "Lane is not set - cannot calculate boundary of car"
+        if self.lane.direction == Direction.north:
             self.rear_right = (rlX + self.width, rlY)
             self.front_left = (rlX, rlY + self.height)
             self.front_right = (rlX + self.width, rlY + self.height)
-        elif self.direction == Direction.south:
+        elif self.lane.direction == Direction.south:
             self.rear_right = (rlX - self.width, rlY)
             self.front_left = (rlX, rlY - self.height)
             self.front_right = (rlX - self.width, rlY - self.height)        
-        elif self.direction == Direction.east:
+        elif self.lane.direction == Direction.east:
             self.rear_right = (rlX, rlY-self.width)
             self.front_left = (rlX+ self.height, rlY )
             self.front_right = (rlX+ self.height, rlY -self.width)        
-        elif self.direction == Direction.west:
+        elif self.lane.direction == Direction.west:
             self.rear_right = (rlX, rlY+self.width)
             self.front_left = (rlX-self.height, rlY )
             self.front_right = (rlX- self.height, rlY +self.width)   
@@ -38,18 +38,23 @@ class Car (Vehicle):
         #Recalculate speed at time step depending on distance2nearestobstacle
         rlX : float = self.rear_left[0]
         rlY : float = self.rear_left[1]
+        assert self.lane is not None, "Lane is not set - cannot move car"
         if self.distance2nearestobstacle()>5:
-            if self.direction == Direction.north:
+            if self.lane.direction == Direction.north:
                 self.rear_left = (rlX, rlY+self.speed)
-            elif self.direction == Direction.south:
+            elif self.lane.direction == Direction.south:
                 self.rear_left = (rlX, rlY-self.speed)
-            elif self.direction == Direction.east:
+            elif self.lane.direction == Direction.east:
                 self.rear_left = (rlX+self.speed, rlY)
-            elif self.direction == Direction.west:
+            elif self.lane.direction == Direction.west:
                 self.rear_left = (rlX-self.speed, rlY)
         self.findBoundaries()
 
+    def setLane(self, lane):
+        self.lane = lane
 
     def distance2nearestobstacle(self):
         #TODO
         pass
+
+    
--- sim/lane.py
+++ sim/lane.py
@@ -1,4 +1,7 @@
+from sim.vehicle import Vehicle
 from consts import *
+from car import Car
+import heapq
 
 class Lane:
     def __init__(
@@ -14,10 +17,26 @@ class Lane:
         self.width = width
         self.start = start
         self.end = end
+        # Vehicle list sorted depending on direction - furthest car on road is always first
         self.vehicles = []
         self.min = self.position - self.width / 2
         self.max = self.position + self.width / 2
 
-    def add_vehicle(self, ):
-        # TODO
-        pass
+    def add_vehicle(self, vehicle: Car):
+        # Ensure vehicle is in the lane
+        if self.direction == Direction.north or self.direction == Direction.south:
+            assert (self.min <= vehicle.rear_left[0] <= self.max) and (self.min <= vehicle.rear_right[0] <= self.max)
+        elif self.direction == Direction.west or self.direction == Direction.east:
+            assert (self.min <= vehicle.rear_left[1] <= self.max) and (self.min <= vehicle.rear_right[1] <= self.max)
+        # Set lane in vehicle so it knows where it is.
+        vehicle.setLane(self)
+        vehicle.findBoundaries()
+        # Add to the sorted vehicle list (sorted depending on lane direction)
+        if self.direction == Direction.north:
+            heapq.heappush(self.vehicles, (-vehicle.rear_left[1],vehicle))
+        elif self.direction == Direction.south:
+            heapq.heappush(self.vehicles, (vehicle.rear_left[1], vehicle))
+        elif self.direction == Direction.east:
+            heapq.heappush(self.vehicles, (-vehicle.rear_left[0],vehicle))
+        elif self.direction == Direction.west:
+            heapq.heappush(self.vehicles, (vehicle.rear_left[0],vehicle))
