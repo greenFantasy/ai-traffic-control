9c82726962a1afc107b2af70c412b66ed17fda88
Rajat Mittal <rajatmittal@college.harvard.edu>
2021-09-05 15:26:39 -0400

    stuff

--- sim/trafficlight.py
+++ sim/trafficlight.py
@@ -8,6 +8,9 @@ class TrafficLight:
         self.intersection = intersection
         self.state: TrafficLightStates = TrafficLightStates.red
         self.state_start_time: float = self.get_current_time()
+        self.pos_x = None
+        self.pos_y = None
+        self.sensor: Sensor = None
 
     def green_to_yellow(self):
         assert(self.state == TrafficLightStates.green)
@@ -24,11 +27,11 @@ class TrafficLight:
         self.state = TrafficLightStates.green
         self.state_start_time = self.get_current_time()
 
-    def get_sensor_data(self, sensor_type=SensorTypes.camera):
+    def get_sensor_data(self):
         lanes = self.intersection.street_dict[self.street_direction].lanes
         data = []
         for l in lanes:
-            data.extend(l.vehicles) # TODO: filter out for vehicles in a certain range
+            data.extend(l.get_vehicles()) # TODO: filter out for vehicles in a certain range
 
     def get_current_time(self):
         # TODO, likely will need to go in a different file
