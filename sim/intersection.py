8a60eb20678c44994fe537c177e9216e9013482b
Rajat Mittal <rajatmittal@college.harvard.edu>
2021-09-14 17:27:42 -0400

    removing directions from classes

--- sim/intersection.py
+++ sim/intersection.py
@@ -32,20 +32,21 @@ class Intersection:
 
     def _determine_boundaries(self):
         # TODO(rajatmittal): how to determine boundaries
-        self.left_boundary = min([street.min for street in self.streets
-                                  if Direction.is_north_south(street.direction)])
-        self.right_boundary = max([street.max for street in self.streets
-                                   if Direction.is_north_south(street.direction)])
-        self.lower_boundary = min([street.min for street in self.streets
-                                  if not Direction.is_north_south(street.direction)])
-        self.upper_boundary = max([street.max for street in self.streets
-                                  if not Direction.is_north_south(street.direction)])
+        pass
+        # self.left_boundary = min([street.min for street in self.streets
+        #                           if Direction.is_north_south(street.direction)])
+        # self.right_boundary = max([street.max for street in self.streets
+        #                            if Direction.is_north_south(street.direction)])
+        # self.lower_boundary = min([street.min for street in self.streets
+        #                           if not Direction.is_north_south(street.direction)])
+        # self.upper_boundary = max([street.max for street in self.streets
+        #                           if not Direction.is_north_south(street.direction)])
 
     def _create_paths_in_intersection(self):
         for (inbound, outbound, moveOp) in self.paths_to_connect:
             inboundEnd = inbound.end
             outboundStart = outbound.start
-            subPath = Path(parametrization = LinearParam(inboundEnd, outboundStart)) 
+            subPath = Path(parametrization = LinearParam(inboundEnd, outboundStart))
             inbound.add_connecting_path(subPath)
             subPath.add_connecting_path(outbound)
             # get traffic light corresponding to this path movement
@@ -54,6 +55,6 @@ class Intersection:
 
     def _create_traffic_lights(self):
         for street in self.streets:
-            self.traffic_lights[(street, MovementOptions.left)] = TrafficLight(MovementOptions.left, street.direction, self, sensor=Camera(range=30.0))
-            self.traffic_lights[(street, MovementOptions.through)] = TrafficLight(MovementOptions.through, street.direction, self, sensor=Camera(range=30.0))
-            self.traffic_lights[(street, MovementOptions.right)] = TrafficLight(MovementOptions.right, street.direction, self, sensor=Camera(range=30.0))
+            self.traffic_lights[(street.id, MovementOptions.left)] = TrafficLight(MovementOptions.left, street.id, self, sensor=Camera(range=30.0))
+            self.traffic_lights[(street.id, MovementOptions.through)] = TrafficLight(MovementOptions.through, street.id, self, sensor=Camera(range=30.0))
+            self.traffic_lights[(street.id, MovementOptions.right)] = TrafficLight(MovementOptions.right, street.id, self, sensor=Camera(range=30.0))
