6b556c2ff6b8a99e4e63a73240f6f7efc47dcf1b
Rajat Mittal <rajatmittal@college.harvard.edu>
2021-12-08 15:39:15 -0500

    improved model, changed reward function, support for sgd optimizer, added biger layers to model, added features to environment state

--- util/graph_cars_through.py
+++ util/graph_cars_through.py
@@ -7,3 +7,24 @@
 # f.close()
 
 # plt.plot(cars_through)
+import sys
+sys.path.append("/Users/rajatmittal/Documents/Projects/ai-traffic-control/RL")
+from modelcreator import StateActionNetwork
+
+import torch
+import dill 
+
+path = "/Users/rajatmittal/Documents/Projects/ai-traffic-control/RL/traindata/2021_12_08_12_56_02_run1577.pkl"
+with open(path, "rb") as file:
+    sars = dill.load(file)
+
+model = torch.load("/Users/rajatmittal/Documents/Projects/ai-traffic-control/RL/model.pt")
+
+
+for idx in range(len(sars)):
+    print("State: ", sars[idx][0])
+    print("Action: ", sars[idx][1])
+    print("Reward: ", sars[idx][2])
+    print("Current (Not time of Sim) Model Predictions: ", model.forward(sars[idx][0].unsqueeze(0)))
+    print()
+    print()
