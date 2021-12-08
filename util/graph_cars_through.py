a0cb666107646e218c6cdfa1a04ac920c03fc747
sss04 <sssai@andrew.cmu.edu>
2021-12-08 14:53:13 -0600

    create graph_cars_through

--- util/graph_cars_through.py
+++ util/graph_cars_through.py
@@ -1,30 +1,42 @@
-# import matplotlib.pyplot as plt
-
-# cars_through = 
-
-# f = open("data/vehicle_despawn.csv", "r")
-# cars_through.append(len(f.readlines())) # len(pd.read_csv("data/vehicle_despawn.csv"))
-# f.close()
-
-# plt.plot(cars_through)
+import matplotlib.pyplot as plt
 import sys
-sys.path.append("/Users/rajatmittal/Documents/Projects/ai-traffic-control/RL")
-from modelcreator import StateActionNetwork
+import os
+import numpy as np
+# sys.path.append("/Users/rajatmittal/Documents/Projects/ai-traffic-control/RL")
+# from modelcreator import StateActionNetwork
 
-import torch
-import dill 
+sim_data_dir = "../data"
+dirnames = os.listdir(sim_data_dir)
+plotting_tups = []
+for dirname in dirnames:
+    if "." in dirname:
+        continue
+    idx = int(dirname.split("_")[-1][3:])
+    filepath = os.path.join(sim_data_dir, dirname, "vehicle_despawn.csv")
+    f = open(filepath, "r")
+    numCars = len(f.readlines())
+    f.close()
+    plotting_tups.append((idx, numCars))
+plotting_tups.sort(key=lambda x: x[0])
+plt.plot([elem[0] for elem in plotting_tups], [elem[1] for elem in plotting_tups])
+# plt.show()
+plt.plot([elem[0] for elem in plotting_tups], np.convolve([elem[1] for elem in plotting_tups], np.ones(10)/10, mode='same'), "r")
+plt.show()
 
-path = "/Users/rajatmittal/Documents/Projects/ai-traffic-control/RL/traindata/2021_12_08_12_56_02_run1577.pkl"
-with open(path, "rb") as file:
-    sars = dill.load(file)
+# import torch
+# import dill 
 
-model = torch.load("/Users/rajatmittal/Documents/Projects/ai-traffic-control/RL/model.pt")
+# path = "/Users/rajatmittal/Documents/Projects/ai-traffic-control/RL/traindata/2021_12_08_12_56_02_run1577.pkl"
+# with open(path, "rb") as file:
+#     sars = dill.load(file)
+
+# model = torch.load("/Users/rajatmittal/Documents/Projects/ai-traffic-control/RL/model.pt")
 
 
-for idx in range(len(sars)):
-    print("State: ", sars[idx][0])
-    print("Action: ", sars[idx][1])
-    print("Reward: ", sars[idx][2])
-    print("Current (Not time of Sim) Model Predictions: ", model.forward(sars[idx][0].unsqueeze(0)))
-    print()
-    print()
+# for idx in range(len(sars)):
+#     print("State: ", sars[idx][0])
+#     print("Action: ", sars[idx][1])
+#     print("Reward: ", sars[idx][2])
+#     print("Current (Not time of Sim) Model Predictions: ", model.forward(sars[idx][0].unsqueeze(0)))
+#     print()
+#     print()
